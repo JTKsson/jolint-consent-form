c1cd5abcee28ce4bc194258387eea7120b3aaed0
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-10-17 12:14:09 +0200

    array of objects

---
+++ src/app/components/OverviewBox/index.jsx
@@ -0,0 +1,39 @@
+const BoxData = [
+    {
+        icon: 
+        box:
+        title: 'Introduction',
+        text: 'Let Jolint introduce themsselves to you'
+    }
+    {
+        icon: 
+        box:
+        title: 'Purpose',
+        text: 'The purpose of having Jolint in your company'
+    }
+    {
+        icon: 
+        box:
+        title: 'Your Rights',
+        text: 'Your rights with the processing of your personal data'
+    }
+    {
+        icon: 
+        box:
+        title: 'Consent',
+        text: 'Sign to improve inclusion and belonging in your company'
+    }
+]
+
+
+
+const OverviewBox = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default OverviewBox
