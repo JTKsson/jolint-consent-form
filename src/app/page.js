b4d95ff39bb3bc9264cda79275a2f568daa39a4b
CyberGalactus <edu.garma97@hotmail.com>
2023-10-18 18:06:45 +0200

    info ticket in wrong repo

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
-import Navbar from "./components/Navbar"
 import MainContent from "./components/MainContent"
-
 export default function Home() {
   return (
     <>
-      <Navbar />
       <MainContent />
     </>
   )
