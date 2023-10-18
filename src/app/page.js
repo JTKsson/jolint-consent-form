3e0abf008e04a6dcc47bd9380bc59ea3ad7dc9c5
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-10-18 15:55:06 +0200

    add Heading component to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import { Heading } from "./components/Heading"
 import MainContent from "./components/MainContent"
+
+
 export default function Home() {
   return (
     <>
+    <Heading />
       <MainContent />
     </>
   )
