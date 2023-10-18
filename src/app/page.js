d1b75abb77f4416ae888551aa7c0066194751e45
CyberGalactus <edu.garma97@hotmail.com>
2023-10-18 17:35:19 +0200

    navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar"
 import MainContent from "./components/MainContent"
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <MainContent />
     </>
   )
