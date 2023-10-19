875fb3bae4e67af1a0f1f3bb91f97777a8e61322
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-10-19 10:55:33 +0200

    quick fix pull conflict

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 import Header from './components/Header'
-=======
 import { Heading } from './components/Heading'
 import MainContent from './components/MainContent'
->>>>>>> main
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <Header />
-      </div>
-=======
+      <Header />
       <Heading />
       <MainContent />
->>>>>>> main
     </>
   )
 }
