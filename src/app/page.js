5946755fe4428cfc046d393214f72c81d179d4d6
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-10-19 13:51:47 +0200

    weird conflict fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import Header from './components/Header'
-=======
 import { Heading } from './components/Heading'
 import MainContent from './components/MainContent'
->>>>>>> main
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <Header />
-      </div>
-=======
       <Heading />
       <MainContent />
->>>>>>> main
     </>
   )
 }
