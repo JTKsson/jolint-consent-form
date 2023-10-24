64613db94ab1d80017d3d43070ffdc3a51a25890
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-10-24 16:43:42 +0200

    add conditional rendering for pageTypes and buttonTypes

--- src/app/components/MainContent/index.jsx
+++ src/app/components/MainContent/index.jsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import formCopy from '@/app/assets/formCopy'
 import { OverviewPage } from '../OverviewPage'
-import { InfoPage } from '../InfoPage'
+import ThankYou from '../ThankYou'
 
 export const MainContent = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
@@ -12,16 +12,16 @@ export const MainContent = () => {
     <>
       <div>
         {formCopy[currentIndex].pageType === 'overview' && <OverviewPage />}
-        {formCopy[currentIndex].pageType === 'info' && 
+        {formCopy[currentIndex].pageType === 'info' && (
           <>
             <h2>{formCopy[currentIndex].subtitle}</h2>
             <p>{formCopy[currentIndex].content}</p>
           </>
-        }
-        
+        )}
+        {formCopy[currentIndex].pageType === 'thanks' && <ThankYou />}
       </div>
 
-      {currentIndex !== 0 && (
+      {currentIndex !== 0 && currentIndex !== formCopy.length - 1 && (
         <button
           onClick={() => setCurrentIndex(currentIndex - 1)}
           disabled={currentIndex === 0}
@@ -29,12 +29,23 @@ export const MainContent = () => {
           Back
         </button>
       )}
-      {currentIndex !== formCopy.length - 1 && (
-        <button
-          onClick={() => setCurrentIndex(currentIndex + 1)}
-          disabled={currentIndex === formCopy.length - 1}
-        >
-          {currentIndex === 0 ? 'Start' : 'Next'}
+      {currentIndex !== formCopy.length - 1 &&
+        formCopy[currentIndex].buttonType !== 'submit' &&
+        formCopy[currentIndex].buttonType !== 'done' &&
+        formCopy[currentIndex].pageType !== 'thanks' && (
+          <button
+            onClick={() => setCurrentIndex(currentIndex + 1)}
+            disabled={currentIndex === formCopy.length - 1}
+          >
+            {currentIndex === 0 ? 'Start' : 'Next'}
+          </button>
+        )}
+      {formCopy[currentIndex].buttonType === 'done' && (
+        <button onClick={() => setCurrentIndex(currentIndex + 1)}>Done</button>
+      )}
+      {formCopy[currentIndex].buttonType === 'submit' && (
+        <button onClick={() => setCurrentIndex(currentIndex + 1)}>
+          Submit
         </button>
       )}
     </>
