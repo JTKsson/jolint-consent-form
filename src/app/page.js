64d68c9636403cfe16829115b60e09f049618fa0
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-10-25 11:22:50 +0200

    apply prettier to all files

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer/";
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer/'
 import { Heading } from './components/Heading'
-
 
 export default function Home() {
   return (
