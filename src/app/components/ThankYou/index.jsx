5fe533818434c25d40cc67178d99fb8e056f3548
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-10-25 10:26:47 +0200

    remove components to see the style

--- src/app/components/ThankYou/index.jsx
+++ src/app/components/ThankYou/index.jsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
-import checkActive from '../../../../public/checkActive.jpg'
+import checkActive from '../../../../public/checkround.svg'
 import Link from 'next/link'
+import Styles from "./thankyou.module.css";
 
 const ThankYou = () => {
   return (
-    <>
-      <div>
+    <div className={Styles.thanks}>
+      
         <Image
           src={checkActive}
           alt="Icon of check being active"
@@ -13,7 +14,6 @@ const ThankYou = () => {
           height={51}
         />
         <h1>Thank you!</h1>
-      </div>
       <p>
         Your consent form was successfully submitted. We appreciate your
         collaboration. Thank you!
@@ -21,7 +21,7 @@ const ThankYou = () => {
       <Link href="https://jolint.com/">
         <button>Close</button>
       </Link>
-    </>
+    </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,14 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer/";
 import { Heading } from './components/Heading'
+import ThankYou from "./components/ThankYou";
 
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Heading />
-      <MainContent />
+      <ThankYou />
       <Footer />
     </>
   )
