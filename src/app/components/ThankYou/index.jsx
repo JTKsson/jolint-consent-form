d0deff7a0ec69620c179a96f009abf1bd6b31b67
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-10-26 14:13:32 +0200

    update code

--- src/app/components/ThankYou/index.jsx
+++ src/app/components/ThankYou/index.jsx
@@ -6,7 +6,7 @@ import Styles from './thankyou.module.css'
 const ThankYou = () => {
   return (
     <div className={Styles.thanks}>
-      <div>
+      <div className={Styles.picture}>
         <Image
           src={checkActive}
           alt="Icon of check being active"
