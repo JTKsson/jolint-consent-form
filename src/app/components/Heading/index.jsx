aa4fbb7f84f01271e2bf529a019a6c70db6638a4
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-10-30 16:22:09 +0100

    style in heading form

--- src/app/components/Heading/index.jsx
+++ src/app/components/Heading/index.jsx
@@ -1,13 +1,13 @@
 import speakerIcon from '../../assets/images/speaker-icon.svg'
-
 import Image from 'next/image'
+import Styles from './heading.module.css'
 
 export const Heading = () => {
   const icon = speakerIcon
 
   return (
-    <div>
-      <h1>Consent Form for Data Processing</h1>
+    <div className={Styles.main}>
+      <h1 className={Styles.title}>Consent Form for Data Processing</h1>
       <div>
         <Image src={icon} alt="screen reader button" />
       </div>
