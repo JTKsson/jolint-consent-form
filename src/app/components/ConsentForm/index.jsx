7a71403d4e10dcbd0e9a93a764ae97cf2b50cc17
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-11-07 21:54:30 +0100

    run prettier

--- src/app/components/ConsentForm/index.jsx
+++ src/app/components/ConsentForm/index.jsx
@@ -1,29 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { format } from 'date-fns'
 
-
-
 const ConsentForm = () => {
-
-  const time = new Date().toLocaleTimeString();
-  const [currentTime, setCurrentTime] = useState(time);
+  const time = new Date().toLocaleTimeString()
+  const [currentTime, setCurrentTime] = useState(time)
 
   const updateTime = () => {
-    let time = new Date(). toLocaleTimeString();
-    setCurrentTime(time);
+    let time = new Date().toLocaleTimeString()
+    setCurrentTime(time)
   }
 
-  setInterval(updateTime, 1000);
-  
+  setInterval(updateTime, 1000)
+
   return (
     <div>
       <form>
+        <p>Today, on {format(new Date(), 'EEEE d MMM yyyy H:mm:ss')}</p>
         <p>
-          Today, on {format(new Date(), 'EEEE d MMM yyyy H:mm:ss')}
-        </p>
-        <p>
-        <span><input type="checkbox" name="consent" value="no" id="consent" unchecked required/></span>
-        I, <input type="text" id="fname" name="name" required placeholder='enter your name'/>  hereby consent to the processing of my personal data as described before.
+          <span>
+            <input
+              type="checkbox"
+              name="consent"
+              value="no"
+              id="consent"
+              unchecked
+              required
+            />
+          </span>
+          I,{' '}
+          <input
+            type="text"
+            id="fname"
+            name="name"
+            required
+            placeholder="enter your name"
+          />{' '}
+          hereby consent to the processing of my personal data as described
+          before.
         </p>
       </form>
     </div>
