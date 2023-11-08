48223a84626852a569fddf469e99e1fde5903938
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-11-08 10:46:10 +0100

    run prettier

--- src/app/components/PageIndicator/index.jsx
+++ src/app/components/PageIndicator/index.jsx
@@ -1,23 +1,40 @@
-import { PiCircleDuotone } from "react-icons/pi";
+import { PiCircleDuotone } from 'react-icons/pi'
 import Styles from './pageIndicator.module.css'
 
 const PageIndicator = () => {
-    return(
-        <div className={Styles.page}>
-            <ul className={Styles.circles}>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots}/></li>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots} /></li>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots} /></li>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots} /></li>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots} /></li>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots} /></li>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots} /></li>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots} /></li>
-                <li className={Styles.list}><PiCircleDuotone  className={Styles.dots} /></li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className={Styles.page}>
+      <ul className={Styles.circles}>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+        <li className={Styles.list}>
+          <PiCircleDuotone className={Styles.dots} />
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default PageIndicator
-
