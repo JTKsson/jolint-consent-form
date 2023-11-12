6302258522ace81dfcbcf1f7304bdee1ba0ad567
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-11-12 16:18:52 +0100

    fuck vercel v.11

--- src/app/components/OverviewBox/index.jsx
+++ src/app/components/OverviewBox/index.jsx
@@ -1,43 +1,33 @@
-// 'use client'
 import React, { useEffect, useState } from 'react'
 import { unlockBox, checkBox } from './progressFunctions'
 import boxData from './boxDatas'
-
 import Image from 'next/image'
 import Styles from './overviewBox.module.css'
 
 const OverviewBox = ({ currentIndex }) => {
-  const [boxDatas, setBoxDatas] = useState(getInitialBoxDatas())
-
-  // const isLocalStorageAvailable =
-  //    && window.localStorage
-
-  // const storedBoxDatas = isLocalStorageAvailable
-  //   ? JSON.parse(window.localStorage.getItem('boxDatas')) || boxData
-  //   : boxData
-
+  const [boxDatas, setBoxDatas] = useState(() => getInitialBoxDatas());
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       if (currentIndex === 0) {
-        setBoxDatas((prevBoxDatas) => unlockBox(0, true, prevBoxDatas))
+        setBoxDatas((prevBoxDatas) => unlockBox(0, true, prevBoxDatas));
       } else if (currentIndex === 3) {
         setBoxDatas((prevBoxDatas) =>
           checkBox(0, true, unlockBox(1, true, prevBoxDatas)),
-        )
+        );
       } else if (currentIndex === 5) {
         setBoxDatas((prevBoxDatas) =>
           checkBox(1, true, unlockBox(2, true, prevBoxDatas)),
-        )
+        );
       } else if (currentIndex === 9) {
         setBoxDatas((prevBoxDatas) =>
           checkBox(2, true, unlockBox(3, true, prevBoxDatas)),
-        )
+        );
       } else if (currentIndex === 13) {
-        setBoxDatas((prevBoxDatas) => checkBox(3, true, prevBoxDatas))
+        setBoxDatas((prevBoxDatas) => checkBox(3, true, prevBoxDatas));
       }
     }
-  }, [currentIndex])
+  }, [currentIndex]);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -46,19 +36,14 @@ const OverviewBox = ({ currentIndex }) => {
   }, [boxDatas]);
 
   function getInitialBoxDatas() {
-    // Load from local storage or use default data
-    if (typeof window !== 'undefined') {
-      const storedBoxDatas = JSON.parse(localStorage.getItem('boxDatas')) || boxData;
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const storedBoxDatas =
+        JSON.parse(localStorage.getItem('boxDatas')) || boxData;
       return storedBoxDatas;
     }
-    return boxData; // Fallback for SSR
+    return boxData;
   }
 
-  // const [isMounted, setIsMounted] = useState(false)
-  // useEffect(() => {
-  //   setIsMounted(true)
-  // }, [])
-  // if (!isMounted) return null
 
   return (
     <div className={Styles.container}>
